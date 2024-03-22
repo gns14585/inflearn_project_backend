1ea570176259f99fc2611eb46af8ef4bc1701a54
LeeJungHoon <gns14585@naver.com>
2024-03-22 16:03:13 +0900

    commit

---
+++ src/main/java/com/example/react_project_backend/ReactProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactProjectBackendApplication.class, args);
+    }
+
+}
