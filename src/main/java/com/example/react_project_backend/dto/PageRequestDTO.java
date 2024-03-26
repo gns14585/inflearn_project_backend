05aaec4c07cfa60dbce4508a38e1f6a3a6af5467
LeeJungHoon <gns14585@naver.com>
2024-03-26 16:57:12 +0900

    페이징처리

--- src/main/java/com/example/react_project_backend/dto/PageRequestDTO.java
+++ src/main/java/com/example/react_project_backend/dto/PageRequestDTO.java
@@ -1,9 +1,16 @@
 package com.example.react_project_backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder // 상속을 해야할 경우
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
--- src/main/java/com/example/react_project_backend/dto/PageResponseDTO.java
+++ src/main/java/com/example/react_project_backend/dto/PageResponseDTO.java
@@ -1,7 +1,44 @@
 package com.example.react_project_backend.dto;
 
-public class PageResponseDTO<E> {
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+public class PageResponseDTO<E> { // 페이징 결과물
+
+    private List<E> dtoList;
+    private List<Integer> pageNumList;
+    private PageRequestDTO pageRequestDTO;
+    private boolean prev, next;
+    private int totalCount, prevPage, nextPage, totalPage, current;
+
+    // 페이징 처리
+    public PageResponseDTO(List<E> dtoList, PageRequestDTO pageRequestDTO, long total) {
+        this.dtoList = dtoList;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int)total;
+
+        // 끝페이지 계산 end
+        // 현재페이지번호
+        int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
+
+        // 시작페이지번호
+        int start = end - 9;
+
+        // 진짜 마지막페이지 계산
+        int last = (int)(Math.ceil(totalCount / (double) pageRequestDTO.getSize()));
+
+        end = end > last ? last : end;
+        this.prev = start > 1;
+        this.next = totalCount > end * pageRequestDTO.getSize();
+        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        this.prevPage = prev ? start - 1 : 0; // 이전페이지 관련
+        this.nextPage = next ? end + 1 : 0; // 다음페이지 관련
 
 
+    }
 
 }
