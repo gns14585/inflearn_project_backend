84ad14314ec1b6b28b267c55f8d4d89873bea7c5
LeeJungHoon <gns14585@naver.com>
2024-03-27 20:13:33 +0900

    파일 업로드 기능 구현중

---
+++ src/main/java/com/example/react_project_backend/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.example.react_project_backend.controller;
+
+import com.example.react_project_backend.dto.ProductDTO;
+import com.example.react_project_backend.util.CustomFileUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final CustomFileUtil fileUtil;
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO) {
+
+        log.info("register: " + productDTO);
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> uploadedFileNames = fileUtil.saveFiles(files);
+        productDTO.setUploadedFileNames(uploadedFileNames);
+        log.info(uploadedFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
