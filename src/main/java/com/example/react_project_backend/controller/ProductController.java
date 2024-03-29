e15f961d0705a643bb0f66cc67d549bc9e1c411c
LeeJungHoon <gns14585@naver.com>
2024-03-29 16:38:58 +0900

    파일 삭제 기능구현중

--- src/main/java/com/example/react_project_backend/controller/ProductController.java
+++ src/main/java/com/example/react_project_backend/controller/ProductController.java
@@ -4,9 +4,9 @@ import com.example.react_project_backend.dto.ProductDTO;
 import com.example.react_project_backend.util.CustomFileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -31,6 +31,15 @@ public class ProductController {
 
         return Map.of("RESULT", "SUCCESS");
     }
+
+    // ----------------------- 파일 조회 ----------------------
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable("fileName") String fileName) {
+
+        return fileUtil.getFile(fileName);
+    }
+
+
 }
 
 
