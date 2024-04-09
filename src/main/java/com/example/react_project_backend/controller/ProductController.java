f134e94dc70b9c6371bd3a8e4f8b6a7628eb279c
LeeJungHoon <gns14585@naver.com>
2024-04-09 21:57:27 +0900

    상품 수정기능, 삭제기능 추가

--- src/main/java/com/example/react_project_backend/controller/ProductController.java
+++ src/main/java/com/example/react_project_backend/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @Log4j2
@@ -67,6 +68,46 @@ public class ProductController {
     @GetMapping("/{pno}")
     public ProductDTO read(@PathVariable("pno") Long pno) {
         return productService.get(pno);
+    }
+
+    @PutMapping("/{pno}")
+    public Map<String, String> modify(@PathVariable Long pno, ProductDTO productDTO) {
+        productDTO.setPno(pno);
+
+        // old product (원래 상품 가져오기)
+        ProductDTO oldProductDTO = productService.get(pno);
+
+        // file upload (새로 업로드하는 파일)
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+
+        // keep files (지워지지 않은 파일들)
+        List<String> uploadedFileNames = productDTO.getUploadFileNames();
+
+        // 파일목록 만들어주기
+        if (currentUploadFileNames != null && !currentUploadFileNames.isEmpty()) {
+            uploadedFileNames.addAll(currentUploadFileNames);
+        }
+
+        productService.modify(productDTO);
+
+        // 기존 파일 삭제
+        List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+        if (oldFileNames != null && !oldFileNames.isEmpty()) {
+            // 원래는 있었는데 새로 업로드된 파일도 아니고, 과거에 업로드 했던 파일도 아닐때
+            List<String> removeFiles = oldFileNames.stream().filter(fileName -> uploadedFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+            fileUtil.deleteFiles(removeFiles);
+
+        }
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{pno}")
+    public Map<String, String> remove(@PathVariable Long pno) {
+        List<String> oldFileNames = productService.get(pno).getUploadFileNames();
+        productService.remove(pno);
+        fileUtil.deleteFiles(oldFileNames);
+        return Map.of("RESULT", "SUCCESS");
     }
 
 
